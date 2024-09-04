d19d3a12a9643a92e0d5313cd9400514ac691628
Mike <educationforlife424@gmail.com>
2024-09-04 19:26:49 +0300

    Done

--- server/server.js
+++ server/server.js
@@ -20,11 +20,11 @@ app.use("/api/doctor", doctorRouter);
 app.use("/api/appointment", appointRouter);
 app.use("/api/notification", notificationRouter);
 app.use("/api/contact",contactroute)
-app.use(express.static(path.join(__dirname, "./client/build")));
+// app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
